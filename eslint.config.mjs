7a652cbde35648c4593f3bbc3f135b96276c29d5
takezaki <takezaki@virtual-tech.net>
2026-08-28 15:25:39 +0900

    微修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,16 @@
-{
-  "extends": ["next/core-web-vitals", "prettier"]
-}
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier/flat'
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+])
